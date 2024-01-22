1716e1f535038545385730cc341f3f8a3eef3088
MattiaPaiano91 <paianomattia1@gmail.com>
2024-01-22 22:14:28 +0100

    completato bonus

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@ import { yugiCards } from './store.js';
 export default {
     data() {
         return {
-            yugiCards
+            yugiCards,
+            loadingElement: false
         };
     },
     components: {
@@ -17,31 +18,57 @@ export default {
         AppFooter
     },  
     methods: {
-
+        showPage(){
+            this.loadingElement = true
+        },
+        ritardaPagina() {
+        setTimeout(() => {
+            this.showPage();
+        }, 3000);}
     },
      created() {
-        axios
+          axios
             .get(this.yugiCards.baseUrl)
             .then((response) => {
                 console.log(response);
                 this.yugiCards.cards = response.data.data;
                 console.log('cards' , this.yugiCards);
-            });
-    }
+                this.ritardaPagina();
+            })
+        
+    },
+    
 }
 </script>
 
 <template>
-
-    <AppHeader />
-    <AppMain
-    />
+    <div class="container">
+        <div v-if="loadingElement == false" class="preLoading-container">
+            <img src="../public/img/640px-Yu-Gi-Oh!_(Logo).jpg" alt="">
+            <p>Caricamento in corso...</p>
+        </div>
+        <div v-else>
+            <AppHeader />
+            <AppMain />
+        </div>
+    </div>
 
 </template>
+  
+
 
 <style lang="scss">
     @use "assets/scss/main" as *;
     @use "assets/scss/partials/variables.scss" as *;
     @import "assets/scss/partials/reset";
+    .preLoading-container{
+        width: 1200px;
+        margin: 0 auto;
+        text-align: center;
+        img{
+            margin: 0 auto;
+        }
+    }
+    
 </style>
 
